c003aca928611b8885ee535a5e00e6b1116896d4
jteshaboev1984-ops <j.teshaboev1984@gmail.com>
2026-09-26 22:26:34 +0500

    chore(exam-prep): refresh written review asset

--- exam-prep/exam-prep-api.js
+++ exam-prep/exam-prep-api.js
@@ -352,7 +352,7 @@
     };
 
     if (src && /exam-prep-api\.js(?:\?|$)/.test(src)) load('script[data-exam-prep-live]', "examPrepLive", "exam-prep-live.js?v=p260results1");
-    load('script[data-exam-prep-written-understanding]', "examPrepWrittenUnderstanding", "exam-prep-written-understanding-ui.js?v=written3");
+    load('script[data-exam-prep-written-understanding]', "examPrepWrittenUnderstanding", "exam-prep-written-understanding-ui.js?v=written4");
     load('script[data-exam-prep-integrity]', "examPrepIntegrity", "exam-prep-integrity.js?v=p243integrity2");
     load('script[data-exam-prep-learner-views]', "examPrepLearnerViews", "exam-prep-learner-views.js?v=p260focus1");
     load('script[data-exam-prep-overview-placement]', "examPrepOverviewPlacement", "exam-prep-overview-placement.js?v=p213placement2");
